fe82597f42a445a585714bfdd6b63b52538cc8c0
Elgun1234 <112864674+Elgun1234@users.noreply.github.com>
2022-09-06 10:44:05 +0100

    Update rod conversion.py

--- github/Brampton Manor Academy/rod conversion.py
+++ github/Brampton Manor Academy/rod conversion.py
@@ -9,23 +9,32 @@ def metres(rods):
     
 def feet(f):
     f = x * 0.3048
-    print(f)
+    
     return(f)
 
 def miles(m):
     m = x * 1609.34
-    print(m)
+    
     return(m)
 
-def furlongs():
+def furlongs(rods):
     h = rods / 40
-    print(h)
+   
     return(h)
+
+def walkspeed(m):
+    ws = m / 60
+    return ws
 
 x = metres(rods)
 print(rods)
 f = feet(x)
-miles(m)
-h = furlongs()
+print(x)
+m = miles(x)
+print(m)
+h = furlongs(rods)
+print(h)
+ws = walkspeed(m)
+print(ws)
 
 
