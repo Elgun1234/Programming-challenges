9767bb7f6dd7b5dfbbb25bc6a37060007da2e440
Elgun1234 <112864674+Elgun1234@users.noreply.github.com>
2022-09-09 13:23:06 +0100

    Update richter.py

--- github/Brampton Manor Academy/richter.py
+++ github/Brampton Manor Academy/richter.py
@@ -1,7 +1,24 @@
-x = float(input("enter richter scale measurement "))
+def richter_input():
+    richter = float(input("enter richter scale measurement: "))
+    return richter
 
-energy = pow(10, 1.5 * x +4.8)
-tnt = energy / 4.184 * pow(10, 9)
+def converter(x):
+    joule = pow(10,1.5*x+4.8)
+    tnt = joule / pow(4.184*10,9)
+    if x == richter:
+        print(f"your richter value {x}")
+    else:
+        print(f"richter: {x}")
+    print(f"equivalence in joules: {joule}")
+    print(f"equivalence in tnt: {tnt}")
+    
 
-print("equivalence in joules : ", energy)
-print("equivalence in tons of tnt", tnt)
+
+
+richter = richter_input()
+converter(1)
+converter(5)
+converter(9.1)
+converter(9.2)
+converter(9.5)
+converter(richter)
