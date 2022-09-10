f334c2f949524b30c847c2a39b4ae0f9998ada88
Elgun1234 <112864674+Elgun1234@users.noreply.github.com>
2022-09-10 11:28:19 +0100

    Update richter.py

--- github/Brampton Manor Academy/richter.py
+++ github/Brampton Manor Academy/richter.py
@@ -1,24 +1,47 @@
 def richter_input():
-    richter = float(input("enter richter scale measurement: "))
+    richter = float(input("\nenter richter scale measurement: "))
     return richter
 
-def converter(x):
+def joule_converter(x):
     joule = pow(10,1.5*x+4.8)
-    tnt = joule / pow(4.184*10,9)
-    if x == richter:
-        print(f"your richter value {x}")
-    else:
-        print(f"richter: {x}")
-    print(f"equivalence in joules: {joule}")
-    print(f"equivalence in tnt: {tnt}")
+    return joule
     
+ 
+def tnt_converter(joule):
+    tnt = joule / pow(10,9)*4.184
+    return tnt
 
 
+richter_j_1 = joule_converter(1)
+richter_t_1 = tnt_converter(richter_j_1)
 
-richter = richter_input()
-converter(1)
-converter(5)
-converter(9.1)
-converter(9.2)
-converter(9.5)
-converter(richter)
+richter_j_5 = joule_converter(5)
+richter_t_5 = tnt_converter(richter_j_5)
+
+richter_j_9_1 = joule_converter(9.1)
+richter_t_9_1 = tnt_converter(richter_j_9_1)
+
+richter_j_9_2 = joule_converter(9.2)
+richter_t_9_2 = tnt_converter(richter_j_9_2)
+
+richter_j_9_5 = joule_converter(9.5)
+richter_t_9_5 = tnt_converter(richter_j_9_5)
+
+r = "Richters"
+j = "Joules"
+t = "Tons of TNT"
+
+print(f"{r:21}{j:36}{t}\n")
+print(f"{1:4} {richter_j_1:28} {richter_t_1:40}")
+print(f"{5:4} {richter_j_5:28} {richter_t_5:38}")
+print(f"{9.1:5} {richter_j_9_1:30} {richter_t_9_1:36}")
+print(f"{9.2:5} {richter_j_9_2:30} {richter_t_9_2:36}")
+print(f"{9.5:5} {richter_j_9_5:31} {richter_t_9_5:34}")
+
+richter = richter_input()    
+joule = joule_converter(richter)
+tnt = tnt_converter(joule)
+
+print(f"Richter value: {richter}")
+print(f"equivalence in joules: {joule}")
+print(f"equivalence in Tons of TNT: {tnt}")
