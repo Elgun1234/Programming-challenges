a12ee20c67adad34171de7f956a1eda7bfda3c06
Elgun1234 <112864674+Elgun1234@users.noreply.github.com>
2022-09-14 19:08:09 +0100

    Update 99 trick test.py

--- github/Brampton Manor Academy/99 trick/99 trick test.py
+++ github/Brampton Manor Academy/99 trick/99 trick test.py
@@ -5,8 +5,8 @@ from trick_99 import *
 
 class MyFirstTests(unittest.TestCase):
 
-    def factor_test(self):
-        self.assertEqual(factor(), 79)
+    def test_factor_equals(self):
+        self.assertEqual((15), friend(99-15))
 
 if __name__ == '__main__':
     unittest.main()
