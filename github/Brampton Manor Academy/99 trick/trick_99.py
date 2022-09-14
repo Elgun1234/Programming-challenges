d9c321078cae3a77fd889aeeebe8fef5bd210877
Elgun1234 <112864674+Elgun1234@users.noreply.github.com>
2022-09-14 19:07:41 +0100

    Update trick_99.py

--- github/Brampton Manor Academy/99 trick/trick_99.py
+++ github/Brampton Manor Academy/99 trick/trick_99.py
@@ -1,27 +1,39 @@
-
-def factor():
+def answer():
     while True :
         answer = int(input("YOU: select a number between 10 and 49 "))
         if answer > 10 and answer <49:
             break
+    return answer
+
+def factor():
     factor = 99 - answer
     return factor
 
-def friend():
+def friend(factor_answer):
     while True:
-        friend = int(input("FRIEND: select a number between 50 and 99 "))
-        if friend >50 and friend <99:
+        friend_inp = int(input("FRIEND: select a number between 50 and 99 "))
+        if friend_inp >50 and friend_inp <99:
             break
-    return friend
-
-def remove_hundred(friend):
-    str(friend)
-    friend[0,1]
-    print(friend)
-            
+    fri = friend_inp + factor_answer
+    hundred_dig = fri // 100
+    
+    for i in range(0,8):
+        
+        if i == hundred_dig :
+            hundred_to_one = hundred_dig * 100
+            fri -= hundred_to_one
+            fri += hundred_dig
+            friend_inp -= fri
+    return friend_inp
+    
+    
+    
 
 if __name__ == '__main__':
-    unittest.main()
+    
+    answer = answer()
+    factor_answer = factor()
+    x = friend(factor_answer)
+    print(f"I said the answer was {answer} and the calculation result is {x}")
+    
 
-factor = factor()
-friend = friend()
