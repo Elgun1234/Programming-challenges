1fdacfe5cf9b639127569cf6c735aa559c89e662
Elgun1234 <112864674+Elgun1234@users.noreply.github.com>
2022-09-27 10:18:32 +0100

    Update singles.py

--- github/Brampton Manor Academy/singles/singles.py
+++ github/Brampton Manor Academy/singles/singles.py
@@ -11,8 +11,12 @@ def scrape(url):
 
 def finder():
     k = 0
-    while k <10:
-        i = 0
+    i = 0
+    pos = "Postion"
+    song = "Song"
+    art = "Artist"
+    print(f"{pos:45} {song:45} {art}\n")
+    while k <10:        
         open_tr = html.find('<tr>', i)
         close_tr = html.find("</tr>", open_tr)
         newstring = html[open_tr:close_tr]
@@ -20,7 +24,7 @@ def finder():
         pos_start = newstring.find(lookingfor)
         pos_end = newstring.find('</', pos_start)
         rank = newstring[pos_start+len(lookingfor):pos_end]
-        i = close_tr
+        i = close_tr 
         lookingfor2 = '/">' 
         name_open = newstring.find('/">')
         name_close = newstring.find('</a>')
@@ -28,8 +32,9 @@ def finder():
         artist_open = newstring.find('/">',name_close)
         artist_close = newstring.find('</a>',artist_open)
         artist = newstring[artist_open+len(lookingfor2):artist_close]
-        print(rank, name, artist)
+        print(f"{rank:>3} {name:>50} {artist:>60}")
         k += 1
+    
 
 if __name__ == "__main__":
     html = scrape(url)
