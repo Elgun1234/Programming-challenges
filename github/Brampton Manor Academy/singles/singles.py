fa348a437e258b12745620f4fdfbd1e2ddbce317
Elgun1234 <112864674+Elgun1234@users.noreply.github.com>
2022-09-27 07:59:59 +0100

    Add files via upload

---
+++ github/Brampton Manor Academy/singles/singles.py
@@ -0,0 +1,38 @@
+import urllib.request
+
+url = "https://www.officialcharts.com/charts/singles-chart"
+
+def scrape(url):
+    fp = urllib.request.urlopen(url)
+    mybytes = fp.read()
+    mystr = mybytes.decode("utf8")
+    fp.close()
+    return mystr
+
+def finder():
+    k = 0
+    while k <10:
+        i = 0
+        open_tr = html.find('<tr>', i)
+        close_tr = html.find("</tr>", open_tr)
+        newstring = html[open_tr:close_tr]
+        lookingfor = '"position">'
+        pos_start = newstring.find(lookingfor)
+        pos_end = newstring.find('</', pos_start)
+        rank = newstring[pos_start+len(lookingfor):pos_end]
+        i = close_tr
+        lookingfor2 = '/">' 
+        name_open = newstring.find('/">')
+        name_close = newstring.find('</a>')
+        name  = newstring[name_open+len(lookingfor2):name_close]
+        artist_open = newstring.find('/">',name_close)
+        artist_close = newstring.find('</a>',artist_open)
+        artist = newstring[artist_open+len(lookingfor2):artist_close]
+        print(rank, name, artist)
+        k += 1
+
+if __name__ == "__main__":
+    html = scrape(url)
+    finder()
+
+    
