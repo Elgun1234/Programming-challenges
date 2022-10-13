56f609ac080f83164f218a502446a40ee2b72b12
Elgun1234 <112864674+Elgun1234@users.noreply.github.com>
2022-10-13 14:56:26 +0100

    Add files via upload

---
+++ github/Brampton Manor Academy/java 3/src/TinyLife.java
@@ -0,0 +1,22 @@
+public class TinyLife {
+    public static void main(String[] args) throws Exception{
+
+    }
+    long oscillator = 0x20272000037303L;
+    long glider = 0x20A0600000000000;
+
+    public static boolean getCell(long world,int col, int row){}
+    public static long setCell(long world, int col, int col, int row, boolean value){}
+    public static void print(long world){
+        System.out.println("-");
+        for (int row = o;row<8;row++){
+            for (int col = 0; col < 8;col++){
+                System.out.print(getCell(world,col,row)?"#":"_");
+            }
+            System.out.println();
+        }
+    }
+    public static int countNeighbours(long world, int col, int row){}
+    public static boolean computeCell(long world,int col, int row){}
+    public static long nextGeneration(long world)
+}
