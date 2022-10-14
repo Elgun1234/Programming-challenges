c1802e8b792bfd0b2d15627aa907dc2c28b4b97c
Elgun1234 <112864674+Elgun1234@users.noreply.github.com>
2022-10-14 12:35:54 +0100

    Update TinyLife.java

--- github/Brampton Manor Academy/java 3/src/TinyLife.java
+++ github/Brampton Manor Academy/java 3/src/TinyLife.java
@@ -3,10 +3,18 @@ public class TinyLife {
 
     }
     long oscillator = 0x20272000037303L;
-    long glider = 0x20A0600000000000;
+    long glider = 0x20A0600000000000L;
 
-    public static boolean getCell(long world,int col, int row){}
-    public static long setCell(long world, int col, int col, int row, boolean value){}
+    public static boolean getCell(long world,int col, int row){
+
+
+
+    }
+    public static long setCell(long world, int col, int col, int row, boolean value){
+
+
+
+    }
     public static void print(long world){
         System.out.println("-");
         for (int row = o;row<8;row++){
@@ -18,5 +26,5 @@ public class TinyLife {
     }
     public static int countNeighbours(long world, int col, int row){}
     public static boolean computeCell(long world,int col, int row){}
-    public static long nextGeneration(long world)
+    public static long nextGeneration(long world){}
 }
